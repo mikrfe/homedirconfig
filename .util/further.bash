92460519755a6a61ce0cdd627b9c57e6f64dd806
Michał Krzysztof Feiler <archiet@wmi.amu.edu.pl>
2019-10-14 16:15:04 +0200

    further.bash: exec instead of forkexec-exit, also more verbosity

--- .util/further.bash
+++ .util/further.bash
@@ -2,15 +2,16 @@ FURTHER_BASH=$(type -p bash)
 if [[ $SHELL != $BASH ]]
 then
     unset FURTHER_BASH
+    >&2 echo "Now it's Bash $BASH (version $BASH_VERSINFO)."
 elif [[ $BASH != $FURTHER_BASH ]]
-then 
-    $FURTHER_BASH
-    exit $?
+then
+    >&2 echo "Swapping Bash $BASH (version $BASH_VERSINFO) for $FURTHER_BASH …" 
+    exec $FURTHER_BASH "$@"
 else
     unset FURTHER_BASH
-    >&2 echo "Staying in login shell."
+    >&2 echo "Login shell already Bash $BASH (version $BASH_VERSINFO)."
 fi
 [ "${BASH_VERSINFO:-0}" -le 4 ] && {
-    >&2 echo "Warning: Staying in bash version $BASH_VERSINFO < 5."
+    >&2 echo "Warning: Staying in Bash $BASH (version $BASH_VERSINFO) < 5."
     (exit 2)
 }
