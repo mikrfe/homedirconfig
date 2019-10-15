740bbe2efb1905271b0c7786e186a56aa39eeb19
Michał Krzysztof Feiler <archiet@wmi.amu.edu.pl>
2019-10-16 00:41:22 +0200

    powershell profile: lsa,ls functions

--- .config/powershell/Microsoft.PowerShell_profile.ps1
+++ .config/powershell/Microsoft.PowerShell_profile.ps1
@@ -8,3 +8,5 @@ function gd_enter {
 function gd {
 	env GIT_DIR="${env:HOME}/.gd" GIT_WORK_DIR="${env:HOME}" git @Args
 }
+function ls { get-childitem @args }
+function lsa { get-childitem -attributes hidden,!hidden @args }
