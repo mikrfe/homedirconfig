06b1956cd1e0b3b9275b48d9d5b85979d4b15680
Michał Krzysztof Feiler <m@mikf.pl>
2020-04-25 04:43:08 +0200

    awesome: lstroke → keycode 47, still issue #1

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -412,7 +412,7 @@ clientkeys = gears.table.join(
             c.minimized = true
         end ,
         {description = "minimize", group = "client"}),
-    awful.key({ modkey,           }, "lstroke",
+    awful.key({ modkey,           }, "#" .. 47,
         function (c)
             c.maximized = not c.maximized
             c:raise()
